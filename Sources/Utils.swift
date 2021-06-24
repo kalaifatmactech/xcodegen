f527331e3181c400156d76559f27d809a22064c8
kalaifatmactech <kalai@fatmactech.com>
2021-06-24 11:32:30 +0530

    Add files via upload

---
+++ Sources/Utils.swift
@@ -0,0 +1,21 @@
+//
+//  Utils.swift
+//  Template
+//
+//  Created by Vijay FMT G on 24/06/21.
+//
+
+import Foundation
+import SwiftUI
+
+func getBackgroundColor(element: ComponentArray)->Color {
+    let styles: ComponentStyles = element.props.styles
+    if(styles.backgroundColor == "") {
+        return Color.white
+    }
+    else {
+        let color: Color = hexStringToUIColor(hex: styles.backgroundColor)
+        return color
+    }
+    
+}
