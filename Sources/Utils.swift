ac02b1159e1ed74ad318bb4e288d120698264d4b
kalaifatmactech <kalai@fatmactech.com>
2021-07-19 13:02:16 +0530

    Update Utils.swift

--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -8,25 +8,35 @@
 import Foundation
 import SwiftUI
 
-func getBackgroundColor(element: ComponentArray)->Color {
-    let styles: ComponentStyles = element.props.styles
-    if(styles.backgroundColor == "") {
+func getBackgroundColor(backgroundColor: String)->Color {
+    if(backgroundColor == "") {
         return Color.white
     }
     else {
-        let color: Color = hexStringToUIColor(hex: styles.backgroundColor)
+        let color: Color = hexStringToUIColor(hex: backgroundColor)
         return color
     }
     
 }
-func getForegroundColor(element: ComponentArray)->Color {
-    let styles: ComponentStyles = element.props.styles
-    if(styles.color == "") {
+func getForegroundColor(color: String)->Color {
+   
+    if(color == "") {
         return Color.blue
     }
     else {
-        let color: Color = hexStringToUIColor(hex: styles.color)
+        let color: Color = hexStringToUIColor(hex: color)
         return color
+    }
+    
+}
+
+func getPosition(position: Position)-> [CGFloat] {
+    
+    if(position.left != 0.0 && position.top != 0.0) {
+        return [position.left, position.top]
+    }
+    else {
+        return [0.0,0.0]
     }
     
 }
